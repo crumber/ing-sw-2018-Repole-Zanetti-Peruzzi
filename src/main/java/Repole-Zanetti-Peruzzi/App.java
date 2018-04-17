3801d4652e5829e4ae23b0b08124a3cd5c365631
Giampiero <giampiero.repole@mail.polimi.it>
2018-04-17 19:24:29 +0200

    add files

---
+++ src/main/java/Repole-Zanetti-Peruzzi/App.java
@@ -0,0 +1,13 @@
+package Repole-Zanetti-Peruzzi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
