89e8f234e066fa9a29363d392e75094d670802a6
Alessandropeo <alessandro1.peruzzi@mail.polimi.it>
2018-04-20 17:17:27 +0200

    modify dicebag

--- src/main/java/RepoleZanettiPeruzzi/Model/DiceBag.java
+++ src/main/java/RepoleZanettiPeruzzi/Model/DiceBag.java
@@ -8,18 +8,13 @@ public class DiceBag {
     private ArrayList<Die> diceBag=new ArrayList<Die>();
 
     public DiceBag(){
-        Die redDie= new Die(Colour.RED);
-        Die yellowDie= new Die(Colour.YELLOW);
-        Die purpleDie= new Die(Colour.PURPLE);
-        Die greenDie= new Die(Colour.GREEN);
-        Die blueDie= new Die(Colour.BLUE);
 
-        for(int i=0;i<90;i++){
-            if(i<=17) diceBag.add(redDie);
-            if(i>17 && i<=35) diceBag.add(yellowDie);
-            if(i>35 && i<=53) diceBag.add(purpleDie);
-            if(i>53 && i<=71) diceBag.add(greenDie);
-            if(i>71 && i<=89) diceBag.add(blueDie);
+        for(int i=0;i<=17;i++){
+            diceBag.add(new Die(Colour.RED));
+            diceBag.add(new Die(Colour.YELLOW));
+            diceBag.add(new Die(Colour.PURPLE));
+            diceBag.add(new Die(Colour.GREEN));
+            diceBag.add(new Die(Colour.BLUE));
         }
     }
 
