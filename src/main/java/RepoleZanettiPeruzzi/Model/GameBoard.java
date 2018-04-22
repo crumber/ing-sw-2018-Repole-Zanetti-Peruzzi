368dee3d5139a86355967205b4c0084433ec7f53
Andrea Zanetti <andrea4.zanetti@mail.polimi.it>
2018-04-22 15:35:23 +0200

    Added GameBoard, Player, ProxyPlayer and RealPlayer Classes

---
+++ src/main/java/RepoleZanettiPeruzzi/Model/GameBoard.java
@@ -0,0 +1,39 @@
+package RepoleZanettiPeruzzi.Model;
+import java.util.ArrayList;
+
+public class GameBoard {
+    ArrayList<RealPlayer> players;
+    ArrayList<Die> diceDraft;
+    //PublicCard publicCards[];
+    //ToolCard toolCards[];
+    DiceBag diceBag;
+    RoundTrack roundTrack;
+    int round;
+    int turn;
+
+    public GameBoard(int nPlayers){
+        players = new ArrayList<RealPlayer>(nPlayers);
+        diceDraft = new ArrayList<Die>();
+        //publicCards = new PublicCard[3];
+        //toolCards = new ToolCard[3];
+        diceBag = new DiceBag();
+        roundTrack = new RoundTrack();
+    }
+
+    public void addPlayer(String playerName){
+        players.add(new RealPlayer(playerName));
+    }
+
+    public void shuffleCards(){
+        /*Deck deck = new Deck("carte.txt");
+
+        for(int i = 0; i < 3; i++){
+            publicCards[i] = deck.drawPublicCard();
+        }
+
+        for(int i = 0; i < 3; i++){
+            toolCards[i] = deck.drawToolCard();
+        }*/
+    }
+
+}
---
+++ src/main/java/RepoleZanettiPeruzzi/Model/RealPlayer.java
@@ -0,0 +1,38 @@
+package RepoleZanettiPeruzzi.Model;
+
+public class RealPlayer implements Player {
+
+    private String name;
+    private Colour secretColour;
+    private int flavorTokens;
+    private Window window;
+
+    public RealPlayer(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void useToolCard(){
+        //chiama una Action useToolCard
+    }
+
+    @Override
+    public void takeDieFromDraft(){
+        //prende il dado attraverso una Action
+    }
+
+    @Override
+    public void takeDiceFromBag(){
+        //prende il dado dal sacchetto attraverso una Action
+    }
+
+    @Override
+    public void rollDice(){
+        //lancia i dadi presi dal sacchetto
+    }
+
+    @Override
+    public void passTurn(){
+        //chiamo una Action PassTurn
+    }
+}
