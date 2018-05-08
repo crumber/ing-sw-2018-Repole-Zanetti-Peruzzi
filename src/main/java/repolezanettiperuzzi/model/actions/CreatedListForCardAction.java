d6eabb1f892790a24715f48e4d6fa42dde005185
Alessandropeo <alessandro1.peruzzi@mail.polimi.it>
2018-05-08 11:23:10 +0200

    commentato come crea la lista per ogni carta dentro l'azione crea lista per tool card

--- src/main/java/repolezanettiperuzzi/model/actions/CreatedListForCardAction.java
+++ src/main/java/repolezanettiperuzzi/model/actions/CreatedListForCardAction.java
@@ -14,9 +14,11 @@ public class CreatedListForCardAction extends Action{
         String[] clientAnswersArray= clientAnswers.split(" ");
         int idToolCard= board.getToolCards(whichToolCard).getId();
 
+        // for all card
         parameterForCard.add(board);
         parameterForCard.add(player);
 
+        //which die on draft and increment or decrement (1 or 0)
         if(idToolCard==1){
 
             for(int i=0; i<2;i++){
@@ -26,6 +28,7 @@ public class CreatedListForCardAction extends Action{
             }
         }
 
+        //add initial position (x,y) and final position (x2, y2)
         if(idToolCard==2 || idToolCard==3){
 
             for(int i=0; i<4;i++){
@@ -35,6 +38,7 @@ public class CreatedListForCardAction extends Action{
             }
         }
 
+        //add initial position (x1,y1) die 1, final position (x2, y2) die 1, add initial position (x3,y3) die 2 and final position (x4, y4) die 2
         if(idToolCard==4){
 
             for(int i=0; i<8;i++){
@@ -44,6 +48,7 @@ public class CreatedListForCardAction extends Action{
             }
         }
 
+        //add which die on draft and which die on track round (which turn and which turn's die)
         if(idToolCard==5 || idToolCard==9){
 
             for(int i=0; i<3;i++){
@@ -53,6 +58,7 @@ public class CreatedListForCardAction extends Action{
             }
         }
 
+        //add which die on draft
         if(idToolCard==6 || idToolCard==10){
 
             for(int i=0; i<1;i++){
@@ -62,6 +68,7 @@ public class CreatedListForCardAction extends Action{
             }
         }
 
+        //add initial position (x1,y1) die 1, final position (x2, y2) die 1, add initial position (x3,y3) die 2 , final position (x4, y4) die 2 and which die on track round (which turn and which turn's die)
         if(idToolCard==12){
 
             for(int i=0; i<10;i++){
