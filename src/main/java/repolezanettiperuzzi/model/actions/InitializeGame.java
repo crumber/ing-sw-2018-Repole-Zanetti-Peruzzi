8c3830b6e606c3134d249fb81be92a31cb23fe21
Giampiero <giampiero.repole@mail.polimi.it>
2018-05-11 17:07:42 +0200

    create new classes for state pattern and an action that initialize the game

---
+++ src/main/java/repolezanettiperuzzi/model/actions/InitializeGame.java
@@ -0,0 +1,10 @@
+package repolezanettiperuzzi.model.actions;
+
+public class InitializeGame extends Action{
+
+    @Override
+    public void doAction(){
+
+        //inizializzo carte pubbliche e carte tool da file di testo tramite un for che utilizza il metodo static della factory
+    }
+}
