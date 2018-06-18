8214080970c541035d553a8369ba8d7d58e99387
Andrea Zanetti <andrea4.zanetti@mail.polimi.it>
2018-06-18 03:24:56 +0200

    Fixed Border GridPane. Renamed Ripples-of-Lights.png to Ripples-of-Light.png. Started to implement RMI Connection.

--- src/main/java/repolezanettiperuzzi/controller/ControllerRMIServer.java
+++ src/main/java/repolezanettiperuzzi/controller/ControllerRMIServer.java
@@ -1,9 +1,12 @@
 package repolezanettiperuzzi.controller;
 
+import repolezanettiperuzzi.common.DynamicPath;
+
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ControllerRMIServer {
 
@@ -14,19 +17,32 @@ public class ControllerRMIServer {
     }
 
     public void startServer() throws RemoteException {
-        System.setProperty("java.security.policy", "RMI/stupid.policy");
+
+        Registry registry = LocateRegistry.createRegistry(1099);
+
+        System.setProperty("java.rmi.server.useCodebaseOnly", "false");
+        System.setProperty("java.rmi.server.logCalls", "true");
+
+        System.setProperty("java.rmi.server.codebase", new DynamicPath("controller/").getPath()+" "+new DynamicPath("common/").getPath()+" "+new DynamicPath("model/").getPath());
+        System.setProperty("java.security.policy", new DynamicPath("RMI/stupid.policy").getPath());
 
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
 
-        System.out.println("Creating new controller");
+
+
+        System.out.println("Creating new RMI Handler");
         HandlerControllerRMI handlerRMI = new HandlerControllerRMI(controller);
+        System.out.println("Exporting HandlerRMI");
+        HandlerStubRMI stub = (HandlerStubRMI) UnicastRemoteObject.exportObject (handlerRMI, 39000);
 
         System.out.println("Binding");
-        Registry registry = LocateRegistry.getRegistry();
-        registry.rebind("controller", handlerRMI);
+        registry.rebind("controller", stub);
 
         System.out.println("Waiting for invocations...");
+
+        //UnicastRemoteObject.unexportObject(ctrlRMI, true);
+        //UnicastRemoteObject.unexportObject(registry, true);
     }
 }
--- src/main/java/repolezanettiperuzzi/view/WindowGenerator.java
+++ src/main/java/repolezanettiperuzzi/view/WindowGenerator.java
@@ -173,6 +173,8 @@ public class WindowGenerator {
             }
         }
 
+        gridWindow.setGridLinesVisible(true);
+
         return gridWindow;
     }
 }
